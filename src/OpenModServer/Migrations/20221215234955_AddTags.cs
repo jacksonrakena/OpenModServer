d1514fc27d604c31e89c233081f785a4b8982074
Jackson <jackson@rakena.co.nz>
2022-12-16 14:14:04 +1300

    database: Add tags to model & migration

---
+++ src/OpenModServer/Migrations/20221215234955_AddTags.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenModServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "tags",
+                table: "mod_listings",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>());
+
+            migrationBuilder.CreateIndex(
+                name: "IX_mod_listings_tags",
+                table: "mod_listings",
+                column: "tags")
+                .Annotation("Npgsql:IndexMethod", "gin");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_mod_listings_tags",
+                table: "mod_listings");
+
+            migrationBuilder.DropColumn(
+                name: "tags",
+                table: "mod_listings");
+        }
+    }
+}
