f8ac127c473dd2f336345aef7adfc79791f87740
Jackson <jackson@rakena.co.nz>
2022-12-16 19:19:32 +1300

    Moved listing cards to a Razor component

---
+++ src/OpenModServer/Pages/Shared/Components/ListingCard/ListingCard.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OpenModServer.Data;
+
+namespace OpenModServer.Pages.Components;
+
+public class ListingCard : ViewComponent
+{
+    public IViewComponentResult Invoke(ModListing listing)
+    {
+        return View("ListingCard", listing);
+    }
+}
